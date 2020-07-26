a326f9fcf3f609f459407976a7909c98c0ce9ccf
fengbingyi <fengbingyi123456@163.com>
2020-07-26 12:15:39 +0800

    add

---
+++ add.c
@@ -0,0 +1,10 @@
+int add(int a,int b)
+{
+	printf("11111111111");
+	printf("11111111111");
+	printf("11111111111");
+	printf("11111111111");
+	printf("11111111111");
+	printf("11111111111");
+	return (a+b);
+}
---
+++ test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	
+	printf("hello world 11111111111111111\n");
+	printf("sum = %d\n",add(100,99);
+	return 0;
+}
