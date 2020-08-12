8e92002b7f8a3581912e8f0f70276856d4cc9e0a
fengbingyi <fengbingyi123456@163.com>
2020-08-12 15:11:51 +0800

    add

--- add.c
+++ add.c
@@ -1,10 +1,4 @@
 int add(int a,int b)
 {
-	printf("11111111111");
-	printf("22222222");
-	printf("11111111111");
-	printf("11111111111");
-	printf("11111111111");
-	printf("11111111111");
-	return (a+b+a);
+	return (a+b);
 }
--- test.c
+++ test.c
@@ -5,6 +5,6 @@ int main(int argc, const char *argv[])
 	
 	printf("hello world 11111111111111111\n");
 	printf("hello world 11111111111111111\n");
-	//printf("sum = %d\n",add(int a,int b);
+	printf("sum = %d\n",add(int a,int b);
 	return 0;
 }
