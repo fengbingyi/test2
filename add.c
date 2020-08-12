d90d3a9c97d3eede836fb79f0bce7f773a37aec4
fengbingyi <fengbingyi123456@163.com>
2020-08-12 14:54:58 +0800

    add  test.c add.c sub.c README.md

--- add.c
+++ add.c
@@ -1,10 +1,10 @@
 int add(int a,int b)
 {
 	printf("11111111111");
+	printf("22222222");
 	printf("11111111111");
 	printf("11111111111");
 	printf("11111111111");
 	printf("11111111111");
-	printf("11111111111");
-	return (a+b);
+	return (a+b+a);
 }
--- test.c
+++ test.c
@@ -4,6 +4,7 @@ int main(int argc, const char *argv[])
 {
 	
 	printf("hello world 11111111111111111\n");
-	printf("sum = %d\n",add(100,99);
+	printf("hello world 11111111111111111\n");
+	//printf("sum = %d\n",add(int a,int b);
 	return 0;
 }
